b8ef1d16d4c19e9917554c8301c793fbea42a97a
saurabh.agarwal <saurabhagarwal2k8@gmail.com>
2026-05-27 16:53:17 +0530

    FC

--- backend/prisma/migrations/20260527111631_mix/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `email_verified_at` on the `application` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
-
-*/
--- AlterTable
-ALTER TABLE `application` MODIFY `email_verified_at` DATETIME NULL,
-    MODIFY `loan_documents_accepted_at` DATETIME(3) NULL;
